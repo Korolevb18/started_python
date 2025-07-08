da6a3fcdcc1f980836b6381be697f625a69b6d38
Boris Korolev <korolev.b@xmail.ru>
2025-07-08 17:10:32 +0530

    Renew test serch in Google

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -1,8 +1,9 @@
 from selene import browser, be, have
 
-
+import time
 browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('html').should(have.text('About this page'))
+browser.element('[name="q"]').should(be.blank).type('fhksdjfklsdjflksdjlfkdf').press_enter()
+time.sleep(20)
+browser.element('html').should(have.text('По запросу fhksdjfklsdjflksdjlfkdf ничего не найдено'))
 
 # browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
