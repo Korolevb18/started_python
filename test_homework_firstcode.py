6c9596e3e48862cfa3154a09a96875a7bd061e39
Boris Korolev <korolev.b@xmail.ru>
2025-08-11 16:19:56 +0300

    NEW HOME WORK WITH FEW TESTS

--- test_homework_firstcode.py
+++ test_homework_firstcode.py
@@ -1,14 +1,9 @@
 
-
-
 def test_greeting():
-    """
-    Напишите программу, которая выводит на экран приветствие.
-    """
     name = "Анна"
     age = 25
-    # TODO Сформируйте нужную строку
-    output = ""
+    output = f"Привет, {name}! Тебе {age} лет."
+    print(output)
 
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
@@ -22,12 +17,12 @@ def test_rectangle():
     a = 10
     b = 20
     # TODO сосчитайте периметр
-    perimeter = 0
+    perimeter = 2 * (a + b)
 
     assert perimeter == 60
 
     # TODO сосчитайте площадь
-    area = 0
+    area = a * b
 
     assert area == 200
 
@@ -37,14 +32,15 @@ def test_circle():
     Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
     Используйте константу PI
     """
+    import math
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = math.pi * (r ** 2)
 
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
+    length = 2 * math.pi * r
 
     assert length == 144.51326206513048
 
@@ -54,7 +50,11 @@ def test_random_list():
     Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
     """
     # TODO создайте список
-    l = []
+    import random
+
+    random_numbers = [random.randint(1, 100) for _ in range(10)]
+    list.sort(random_numbers)
+    print(random_numbers)
 
     assert len(l) == 10
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
@@ -66,6 +66,8 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     # TODO удалите повторяющиеся элементы
+    l = list(set(l))
+    print(l)
 
     assert isinstance(l, list)
     assert len(l) == 10
@@ -81,9 +83,11 @@ def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
-    d = {}
+    d = dict(zip(first, second))
+    print(d)  # {'a': 1, 'b': 2, 'c': 3}
 
     assert isinstance(d, dict)
     assert len(d) == 5
     assert list(d.keys()) == first
     assert list(d.values()) == second
+
